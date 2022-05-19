b62883f2ddc55695417b558d5dd8d1259f7036ec
kyara-code <chiara.ko.26@gmail>
2022-05-19 10:36:52 +0200

    menu transition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MenuComponent, DashboardComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,38 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger('menu', [
+      state(
+        'open',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translateX(-170px)',
+        })
+      ),
+
+      transition('open<=>closed', animate(500)),
+    ]),
+  ],
 })
 export class MenuComponent implements OnInit {
   menuState = 'closed';
+  menu = 'closed';
 
   constructor() {}
 
@@ -17,7 +43,19 @@ export class MenuComponent implements OnInit {
   }
 
   onSlideMenu() {
-    this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
-    console.log(this.menuState);
+    // this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+    if (this.menuState === 'open') {
+      this.menuState = 'closed';
+      setTimeout(() => {
+        this.menu = 'closed';
+        console.log(this.menu);
+      }, 500);
+    } else {
+      this.menu = 'open';
+      setTimeout(() => {
+        this.menuState = 'open';
+      }, 50);
+      console.log(this.menu);
+    }
   }
 }
