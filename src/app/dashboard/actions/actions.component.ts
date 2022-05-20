2dc72659e408b6a12d2f05a4bdc1fe74c84b7eb0
kyara-code <chiara.ko.26@gmail>
2022-05-20 16:30:26 +0200

    popup

--- src/app/dashboard/actions/actions.component.ts
+++ src/app/dashboard/actions/actions.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
-  styleUrls: ['./actions.component.css']
+  styleUrls: ['./actions.component.css'],
 })
 export class ActionsComponent implements OnInit {
+  popUpType: string = '';
+  closedPopUp: boolean = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const action = params['action'];
+      if (action) {
+        this.popUpType = action;
+        this.onOpenPopUp();
+      }
+      console.log(action);
+    });
   }
 
+  onClosePopUp(close: boolean) {
+    this.closedPopUp = true;
+    console.log(this.closedPopUp);
+  }
+
+  onOpenPopUp() {
+    this.closedPopUp = false;
+    console.log(this.closedPopUp);
+  }
 }
--- src/app/dashboard/actions/pop-up/pop-up.component.ts
+++ src/app/dashboard/actions/pop-up/pop-up.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pop-up',
   templateUrl: './pop-up.component.html',
-  styleUrls: ['./pop-up.component.css']
+  styleUrls: ['./pop-up.component.css'],
 })
-export class PopUpComponent implements OnInit {
+export class PopUpComponent implements OnInit, OnDestroy {
+  @Input() action: string = '';
+  @Output() onClose: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  form: FormGroup = new FormGroup({});
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      toIban: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          /^([A-Z]{2}[ \-]?[0-9]{2})(?=(?:[ \-]?[A-Z0-9]){9,30}$)((?:[ \-]?[A-Z0-9]{3,5}){2,7})([ \-]?[A-Z0-9]{1,3})?$/
+          // PHONE NUMBER VAIDATOR:
+          // /^([+]39)?((3[\d]{2})([ ,\-,\/]){0,1}([\d, ]{6,9}))|(((0[\d]{1,4}))([ ,\-,\/]){0,1}([\d, ]{5,10}))$/
+        ),
+      ]),
+      amount: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\$[0-9]+(\.[0-9][0-9])?$/),
+      ]),
+      // la causale non è obbligatoria
+      description: new FormControl(null, [Validators.maxLength(2)]),
+    });
   }
 
+  closeEvent() {
+    this.onClose.emit(true);
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    this.form.reset();
+  }
+
+  ngOnDestroy(): void {}
 }
