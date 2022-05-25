5489096fcd6a6554b4aa55a24d9adc873591f5ea
kyara-code <chiara.ko.26@gmail>
2022-05-25 11:22:51 +0200

    dashboard modifiche

--- src/app/dashboard/actions/pop-up/pop-up.component.ts
+++ src/app/dashboard/actions/pop-up/pop-up.component.ts
@@ -1,3 +1,4 @@
+import { CardService } from './../../card-manage.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {
@@ -22,9 +23,27 @@ export class PopUpComponent implements OnInit, OnDestroy {
   form2: FormGroup = new FormGroup({});
   form3: FormGroup = new FormGroup({});
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  accountTransfer: { saldoUtente: number; iban: string; active: boolean }[] =
+    [];
+
+  cardIsActive: boolean = false;
+
+  constructor(
+    private cardService: CardService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.accountTransfer = this.cardService.arrayCards.filter((card) => {
+      return card !== this.cardService.cardDisplayed;
+    });
+
+    this.cardIsActive = this.cardService.cardDisplayed.active;
+    console.log('card is active:', this.cardIsActive);
+
+    // console.log(this.accountTransfer[0].iban);
+
     this.form1 = new FormGroup({
       toIban: new FormControl(null, [
         Validators.required,
@@ -32,12 +51,12 @@ export class PopUpComponent implements OnInit, OnDestroy {
           /^([A-Z]{2}[ \-]?[0-9]{2})(?=(?:[ \-]?[A-Z0-9]){9,30}$)((?:[ \-]?[A-Z0-9]{3,5}){2,7})([ \-]?[A-Z0-9]{1,3})?$/
         ),
       ]),
-      amount: new FormControl('€', [
+      amount: new FormControl(null, [
         Validators.required,
-        Validators.pattern(/^\€[0-9]+(\.[0-9][0-9])?$/),
+        Validators.pattern(/^\[0-9]+(\.[0-9][0-9])?$/),
       ]),
       // la causale non è obbligatoria
-      description: new FormControl(null, [Validators.maxLength(2)]),
+      description: new FormControl(null, [Validators.maxLength(200)]),
     });
 
     this.form2 = new FormGroup({
@@ -47,18 +66,18 @@ export class PopUpComponent implements OnInit, OnDestroy {
           /^([+]39)?((3[\d]{2})([ ,\-,\/]){0,1}([\d, ]{6,9}))|(((0[\d]{1,4}))([ ,\-,\/]){0,1}([\d, ]{5,10}))$/
         ),
       ]),
-      amount: new FormControl('€', [
+      amount: new FormControl(null, [
         Validators.required,
-        Validators.pattern(/^\€[0-9]+(\.[0-9][0-9])?$/),
+        Validators.pattern(/^\[0-9]+(\.[0-9][0-9])?$/),
       ]),
       // la causale non è obbligatoria
       description: new FormControl(null, [Validators.maxLength(200)]),
     });
 
     this.form3 = new FormGroup({
-      amount: new FormControl('€', [
+      amount: new FormControl(null, [
         Validators.required,
-        Validators.pattern(/^\€[0-9]+(\.[0-9][0-9])?$/),
+        Validators.pattern(/^\[0-9]+(\.[0-9][0-9])?$/),
       ]),
     });
   }
@@ -68,15 +87,73 @@ export class PopUpComponent implements OnInit, OnDestroy {
   }
 
   onSubmit() {
-    if (this.form1) {
+    if (this.form1.valid) {
       console.log(this.form1);
-      this.form1.reset();
-    } else if (this.form2) {
+    } else if (this.form2.valid) {
       console.log(this.form2);
-      this.form2.reset();
-    } else if (this.form3) {
+
+      // logica per la ricarica cellulare (toglie solo soldi, come per il prelievo)
+      if (this.action === 'ricarica') {
+        const newBalance =
+          Math.round(
+            (this.cardService.cardDisplayed.saldoUtente -
+              +this.form2.value.amount) *
+              100
+          ) / 100;
+        if (newBalance >= 0) {
+          this.cardService.cardDisplayed.saldoUtente = newBalance;
+          this.cardService.cardChanged.next({
+            saldoUtente: newBalance,
+            iban: this.cardService.cardDisplayed.iban,
+            active: this.cardService.cardDisplayed.active,
+          });
+        } else {
+          alert(
+            "L'operazione non è andata a buon fine causa saldo insufficiente sulla carta."
+          );
+        }
+      }
+    } else {
       console.log(this.form3);
-      this.form3.reset();
+
+      // logica per il prelievo e il versamento: i dati aggiornati andranno poi salvati sul db(?)
+      if (this.action === 'prelievo') {
+        const newBalance =
+          Math.round(
+            (this.cardService.cardDisplayed.saldoUtente -
+              +this.form3.value.amount) *
+              100
+          ) / 100;
+        if (newBalance >= 0) {
+          this.cardService.cardDisplayed.saldoUtente = newBalance;
+          this.cardService.cardChanged.next({
+            saldoUtente: newBalance,
+            iban: this.cardService.cardDisplayed.iban,
+            active: this.cardService.cardDisplayed.active,
+          });
+        } else {
+          alert(
+            "L'operazione non è andata a buon fine causa saldo insufficiente sulla carta."
+          );
+        }
+      } else if (this.action === 'versamento') {
+        const newBalance =
+          Math.round(
+            (this.cardService.cardDisplayed.saldoUtente +
+              +this.form3.value.amount) *
+              100
+          ) / 100;
+
+        this.cardService.cardDisplayed.saldoUtente = newBalance;
+
+        this.cardService.cardChanged.next({
+          saldoUtente: newBalance,
+          iban: this.cardService.cardDisplayed.iban,
+          active: this.cardService.cardDisplayed.active,
+        });
+      }
+
+      console.log(this.cardService.cardDisplayed.saldoUtente);
     }
 
     this.closeEvent();
