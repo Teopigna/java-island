917fea49e6a8a850e2bb41bc76bad80d3e59720f
kyara-cose <chiara.ko.26@gmail.com>
2022-06-06 10:16:55 +0200

    updates

--- src/app/dashboard/actions/pop-up/pop-up.component.ts
+++ src/app/dashboard/actions/pop-up/pop-up.component.ts
@@ -1,3 +1,4 @@
+import { TransactionService } from 'src/app/services/transaction.service';
 import { AuthService } from './../../../auth/auth.service';
 import { Account } from './../../../shared/account.model';
 import { CardService } from '../../../services/card-manage.service';
@@ -22,8 +23,6 @@ export class PopUpComponent implements OnInit {
   @Input() action: string = '';
   @Output() onClose: EventEmitter<boolean> = new EventEmitter();
 
-  form1: FormGroup = new FormGroup({});
-  form2: FormGroup = new FormGroup({});
   form3: FormGroup = new FormGroup({});
 
   accountTransfer: Account[] = [];
@@ -37,10 +36,7 @@ export class PopUpComponent implements OnInit {
 
   constructor(
     private cardService: CardService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private authService: AuthService,
-    private http: HttpClient
+    private traService: TransactionService
   ) {}
 
   ngOnInit(): void {
@@ -48,42 +44,11 @@ export class PopUpComponent implements OnInit {
       return card !== this.cardService.cardDisplayed;
     });
 
-
     this.cardIsActive = this.cardService.cardDisplayed?.status;
     console.log('card is active:', this.cardIsActive);
-    console.log('current card index: ' +this.cardService.currentIndex)
+    console.log('current card index: ' + this.cardService.currentIndex);
 
     // console.log(this.accountTransfer[0].iban);
-
-    this.form1 = new FormGroup({
-      toIban: new FormControl(null, [
-        Validators.required,
-        // Validators.pattern(
-        //   /^([A-Z]{2}[ \-]?[0-9]{2})(?=(?:[ \-]?[A-Z0-9]){9,30}$)((?:[ \-]?[A-Z0-9]{3,5}){2,7})([ \-]?[A-Z0-9]{1,3})?$/
-        // ),
-      ]),
-      amount: new FormControl(null, [
-        Validators.required,
-        // Validators.pattern(/^\[0-9]+(\.[0-9][0-9])?$/),
-      ]),
-      // la causale non è obbligatoria
-      description: new FormControl(null, [Validators.maxLength(200)]),
-    });
-
-    this.form2 = new FormGroup({
-      toPhoneNumber: new FormControl(null, [
-        Validators.required,
-        // Validators.pattern(
-        //   /^([+]39)?((3[\d]{2})([ ,\-,\/]){0,1}([\d, ]{6,9}))|(((0[\d]{1,4}))([ ,\-,\/]){0,1}([\d, ]{5,10}))$/
-        // ),
-      ]),
-      amount: new FormControl(null, [
-        Validators.required,
-        // Validators.pattern(/^\[0-9]+(\.[0-9][0-9])?$/),
-      ]),
-      // la causale non è obbligatoria
-      description: new FormControl(null, [Validators.maxLength(200)]),
-    });
 
     this.form3 = new FormGroup({
       amount: new FormControl(null, [
@@ -99,78 +64,16 @@ export class PopUpComponent implements OnInit {
 
   onSubmit() {
     // da implementare il bonifico
-    if (this.form1.valid) {
-      console.log(this.form1);
-
-      if (this.action === 'giroconto') {
-      }
-    } else if (this.form2.valid) {
-      console.log(this.form2);
-
-      // logica per la ricarica cellulare (toglie solo soldi, come per il prelievo)
-      if (this.action === 'ricarica') {
-      }
-    } else {
+    if (this.form3.valid) {
       console.log(this.form3);
-
-      const headerDict = {
-        Authorization: this.authService.user.value!.token,
-      };
-
-      const requestOptions = {
-        headers: new HttpHeaders(headerDict),
-      };
 
       // logica per il prelievo e il versamento: i dati aggiornati andranno poi salvati sul db(?)
       if (this.action === 'prelievo') {
         console.log(this.cardService.cardDisplayed?.accountNumber);
         console.log(-+this.form3.value.amount);
 
-        this.http
-          .post<any>(
-            'http://localhost:8765/api/transactions',
-            {
-              accountNumber: this.cardService.cardDisplayed.accountNumber,
-              amount: -this.form3.value.amount,
-            },
-            requestOptions
-          )
-          .subscribe(
-            (response) => {
-              console.log(response);
-              this.cardService.getAccounts().subscribe((cardsList) => {
-                this.cardService.cardChanged.next(
-                  cardsList[this.cardService.currentIndex]
-                );
-              });
-            },
-            (error) => {
-              console.log(error);
-            }
-          );
+        this.traService.postTransaction(this.form3.value.amount);
       } else if (this.action === 'versamento') {
-        this.http
-          .post<any>(
-            'http://localhost:8765/api/transactions',
-            {
-              accountNumber: this.cardService.cardDisplayed.accountNumber,
-              amount: this.form3.value.amount,
-            },
-            requestOptions
-          )
-          .subscribe(
-            (response) => {
-              console.log(response);
-              this.cardService.getAccounts().subscribe((cardsList) => {
-                this.cardService.cardChanged.next(
-                  cardsList[this.cardService.currentIndex]
-                );
-              });
-            },
-            (error) => {
-              console.log(error);
-            }
-          );
       }
     }
 
--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,3 +1,5 @@
+import { CardService } from './card-manage.service';
+import { FormGroup } from '@angular/forms';
 import { Account } from './../shared/account.model';
 import { Transaction } from './../shared/transaction.model';
 import { AuthService } from './../auth/auth.service';
@@ -10,7 +12,11 @@ export class TransactionService {
   transactions: Transaction[] = [];
   transactionsChanged = new BehaviorSubject<Transaction[]>([]);
 
-  constructor(private authService: AuthService, private http: HttpClient) {}
+  constructor(
+    private cardService: CardService,
+    private authService: AuthService,
+    private http: HttpClient
+  ) {}
 
   getTransactions(card: Account) {
     const headerDict = {
@@ -31,6 +37,42 @@ export class TransactionService {
           this.transactions = resData;
           this.transactionsChanged.next(this.transactions);
         })
+      );
+  }
+
+  postTransaction(amount: number) {
+    const headerDict = {
+      Authorization: this.authService.user.value!.token,
+    };
+
+    const requestOptions = {
+      headers: new HttpHeaders(headerDict),
+    };
+
+    const type = amount > 0 ? 1 : 2;
+
+    this.http
+      .post<any>(
+        'http://localhost:8765/api/transactions',
+        {
+          accountNumber: this.cardService.cardDisplayed.accountNumber,
+          type: type,
+          amount: amount,
+        },
+        requestOptions
+      )
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.cardService.getAccounts().subscribe((cardsList) => {
+            this.cardService.cardChanged.next(
+              cardsList[this.cardService.currentIndex]
+            );
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
       );
   }
 }
